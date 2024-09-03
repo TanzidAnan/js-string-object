d76ffed34d697d479785c59aa3103cb417b8b111
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-04 01:01:06 +0600

    Object values

---
+++ values1.js
@@ -0,0 +1,10 @@
+const person ={
+    name: 'Tanzid Anan',
+    age: 21,
+    profecation : 'developer',
+    salary: 20000,
+    married :false,
+}
+
+const values =Object.values(person);
+console.log(values);
