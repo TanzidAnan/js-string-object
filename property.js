1eb2aac561d389c537948ae2cf245ce90c70ca02
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-04 00:09:55 +0600

    object dot

---
+++ property.js
@@ -0,0 +1,11 @@
+const person ={
+    name: 'Tanzid Anan',
+    age: 21,
+    profecation : 'developer',
+    salary: 20000,
+    married :false,
+}
+
+console.log(person.name);
+const inCome =(person.salary);
+console.log(inCome)
