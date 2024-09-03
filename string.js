3f1d88245280f1da3652b8d67ed6864c9914e9fe
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-03 15:11:11 +0600

    count length

---
+++ string.js
@@ -0,0 +1,16 @@
+const countey ="Bangladesh";
+const devesion ='Dhaka';
+const distick ="B-baria";
+const thena =new String('Barishal');
+
+
+// console.log(typeof thena);
+
+const num = [21, 54, 98, 36, 45, 87];
+// for(let i =0; i>num.length; i++){
+//     console.log(i)
+// }
+
+const count = "Dahka";
+console.log(count.length);
+console.log(count[0])
