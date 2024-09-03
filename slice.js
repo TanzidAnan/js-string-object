2809ee233f23a7b7e29aa3e805f17097e1d31bc2
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-03 16:23:00 +0600

    join

--- slice.js
+++ slice.js
@@ -1,3 +1,18 @@
 const name1 ="Tanzid Anan";
 const part =name1.slice(2,9);
-console.log(part)
+// console.log(part)
+
+
+const country ="I am a Bangladesh";
+// console.log(country.split(""))
+// console.log(country.split("a"))
+
+
+const firendsStr ="Tanzid, Anan, Marzan, Emaon, Rabiul, Miraje";
+const firends =firendsStr.split(",");
+// console.log(firends);
+
+
+
+const realFirends =["Tanzid", "Anan"," Marzan", "Emaon", "Rabiul", "Mirajen"];
+console.log(realFirends.join('|'))
