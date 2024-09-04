a8434221416ca06c5d8a3d79c710cf287b5d269b
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-04 23:59:05 +0600

    array concat

--- concat.js
+++ concat.js
@@ -1,4 +1,9 @@
 const fast ="Tanzid";
 const last ="Anan";
-const total =fast.concat(' ').concat(last);
+const total =fast.concat('').concat(last);
 console.log(total);
+
+const fastName =["Tanzid"];
+const lastName =["Anan"];
+const total1 =fastName.concat().concat(lastName);
+console.log(total1)
