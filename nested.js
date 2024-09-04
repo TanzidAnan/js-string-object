630ac0006e7113ddf720dc7703f4a89d2d30403d
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-04 23:53:53 +0600

    array number

---
+++ nested.js
@@ -0,0 +1,19 @@
+const collage ={
+    name: 'Infra',
+    class:['11','12'],
+    events:['scinec fair', 'bijoy debose', '21 feb'],
+    unique:{
+        coler : 'bule'
+    }
+}
+// console.log(collage.unique.coler);
+
+collage.events[1] ="16 december";
+
+console.log(collage.events[1]);
+delete collage.class
+console.log(collage);
+
+
+const number =[54, 5454, 554,654, 12, 455, 898,4545];
+console.log(number)
