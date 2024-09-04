182b82367f5a7133bf1bc13a09e743eed32f66e9
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-05 03:20:40 +0600

    rebarse loop

--- loop.js
+++ loop.js
@@ -3,8 +3,13 @@ for(let name of fastName){
     // console.log(name[0])
 }
 
-for(let i =0 ; i<fastName.length; i++){
-    for(let n of fastName){
-        console.log(n)
-    }
+// for(let i =0 ; i<fastName.length; i++){
+//     for(let n of fastName){
+//         console.log(n)
+//     }
+// }
+
+const lastName =['Tanzid', 'anan', 'marzan', 'melon', 'emon', 'rakib'];
+for(let i= lastName.length -1 ; i>=0; i--){
+    console.log(lastName[i])
 }
