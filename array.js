e8c061c6d33a211d53c9efb9ff297cb54cffcfa2
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-05 01:21:39 +0600

    concat

--- array.js
+++ array.js
@@ -2,4 +2,10 @@ const numbers =[21, 123, 54, 69, 78, 45, 36, 45];
 numbers.push(54);
 numbers.pop()
 numbers.push(2);
-console.log(numbers)
+// console.log(numbers)
+
+const num1 = [22, 21, 45, 54, 36];
+const num2 = [54, 96, 36];
+const num3 = [85, 36, 74, 96, 74];
+const num4 = [85, 36, 74, 96, 74];
+console.log(num1.concat(num2.concat(num3.concat(num4))))
