4adf59948dba72572dad60e306331673e0f4286d
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-05 03:15:52 +0600

    array length and loop

---
+++ loop.js
@@ -0,0 +1,10 @@
+const fastName =['Tanzid', 'anan', 'marzan', 'melon', 'emon', 'rakib'];
+for(let name of fastName){
+    // console.log(name[0])
+}
+
+for(let i =0 ; i<fastName.length; i++){
+    for(let n of fastName){
+        console.log(n)
+    }
+}
