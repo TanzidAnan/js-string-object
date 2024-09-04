2a56e5acb74d1727ab9e5d4997e308270fe9ebf6
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-05 04:36:19 +0600

    array odd number

--- array-push.js
+++ array-push.js
@@ -1,7 +1,7 @@
 const numbers =[25, 65, 78, 45, 54, 21, 44, 99, 88,36, 74];
 let event1 = [];
 for(let item of numbers){
-    if(item % 2 ===0){
+    if(item % 2 !==0){
         event1.push(item)
     }
 }
