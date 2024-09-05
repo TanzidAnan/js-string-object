a12d5620f413aaa4f21b905a798386bf5fb041c6
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-05 18:37:29 +0600

    contnue array loop

---
+++ contenue.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12 ];
+for(let i =0; i< numbers.length; i++){
+    if(numbers[i] === 5){
+
+        continue;
+        
+    }
+    console.log(numbers[i])
+}
