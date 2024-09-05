b4caff513a773b2f859c3a05de87870ba4ef39f1
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-05 18:47:46 +0600

    object values property

--- keys1.js
+++ keys1.js
@@ -4,4 +4,14 @@ const person ={
     age: 25,
 }
 const keys = Object.keys(person);
-console.log(keys)
+// console.log(keys)
+
+
+
+const person1 ={
+    name: 'Tanzid',
+    address: 'Dhaka',
+    age: 25,
+}
+const value = Object.values(person1);
+console.log(value)
